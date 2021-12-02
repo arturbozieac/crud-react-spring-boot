d83bab31e39817de39def3bf4f4f8ef8d4faf4e4
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2021-12-02 14:59:19 +0200

    DEV1 - Fetch github data and represent it using fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
 import "./App.css";
-import WeatherApp from './components/WeatherApp';
+import RestGithub from "./components/RestGithub";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState('');
 
-  return (
-    <div className="App">
-      <WeatherApp/>
-    </div>
-  );
+
+  const fetchData = () => {
+      const url = `https://api.github.com/search/repositories?q=${keyword}`;
+      fetch(url)
+          .then(response => response.json())
+          .then(responseData => {
+              setData(responseData.items);
+          });
+  }
+  const handleChange = (e) => {
+      setKeyword(e.target.value);
+  }
+
+  const tableRows = data.map((item, index) =>
+      <tr key={index}><td>{item.full_name}</td>
+          <td><a href={item.html_url}>{item.html_url}</a></td></tr>);
+
+      return (
+          <div className="App">
+              <input type="text" onChange={handleChange} />
+              <button onClick={fetchData} value={keyword} >fetch</button>
+              <table><tbody>{tableRows}</tbody></table>
+          </div>
+      );
 }
 
 export default App;
