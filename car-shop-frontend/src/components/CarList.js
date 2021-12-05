68440c862566fb0431f7a9b22608cafa321210aa
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2021-12-06 00:19:36 +0200

    DEV1 - Add CSV export

--- car-shop-frontend/src/components/CarList.js
+++ car-shop-frontend/src/components/CarList.js
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AddCar from './AddCar';
 import EditCar from './EditCar';
+import { CSVLink } from 'react-csv';
 
 
 
@@ -124,6 +125,7 @@ class Carlist extends Component {
         return (
             <div>
                 <AddCar addCar={this.addCar} fetchCars={this.fetchCars} />
+                <CSVLink data={this.state.cars} separator=";">Export CSV</CSVLink>
                 <ReactTable data={this.state.cars} columns={columns} filterable={true} />
                 <ToastContainer autoClose={1500} />
             </div>
